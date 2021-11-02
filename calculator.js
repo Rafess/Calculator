5518605d612ff6687d890e34b9fd85dd1d62a63f
Rafes <rafadvcoliveira@gmail.com>
2021-11-02 17:19:57 -0300

    First Commit

---
+++ calculator.js
@@ -0,0 +1,51 @@
+const calculatorModule = (() => {
+//     // let _numbStorage = [];
+//     // let operations = [];
+//     // const sum = (firstNum, secondNum) => firstNum + secondNum;
+//     // const minus = (firstNum, secondNum) => firstNum - secondNum;
+//     // const divide = (firstNum, secondNum) => firstNum / secondNum;
+//     // const multiplicate = (firstNum, secondNum) => firstNum * secondNum;
+//     // const _calcWard = {
+//     //     '+': sum,
+//     //     '-': minus,
+//     //     '/': divide,
+//     //     '*': multiplicate
+//     //         };
+   
+    
+    // function enter() {
+    //     let number =  document.querySelectorAll('#equals');
+    //     let button = document.querySelectorAll("button"); 
+    //     let total = document.querySelectorAll('#total');
+    //     button.onclick = () => {
+    //     total = total + number;
+    //     }
+    // }
+            
+    // function consoleOk() {
+    //     console.log('ok')
+    // }
+    // const equals = () => {
+    // const [firstNum, secondNum] = _numbStorage;
+    // let result =  _calcWard[operator] ? _calcWard[operator](firstNum, secondNum) :
+    //    "Erro, operação inválida";
+    // let finalResult = `${firstNum} ${operator} ${secondNum} = ${result}`;
+    // operations = [...operations, finalResult];
+    // _numbStorage = []
+    // return finalResult
+    // };
+            
+    // const list = () => {
+    //     return `Essa é sua lista de opeações até o momento: ${operations}`
+    // };
+
+    // const reset = () => { 
+    // firstNum=0
+    // secondNum = 0;
+    // _numbStorage = [];
+    // return (`A calulador foi resetada com sucesso`)
+    // }
+
+    return {enter};
+})();
+
